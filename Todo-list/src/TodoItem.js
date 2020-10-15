9c5238130f3fc621408e784c4c7f85b6cb91f3a0
Mihir Shri <coe18b064@iiitdm.ac.in>
2020-10-16 00:09:06 +0530

    Add files via upload

---
+++ Todo-list/src/TodoItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function TodoItem(props) {
+    const completedStyle = {
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
+
+    return (
+        <div className="todo-item">
+            <input
+                type="checkbox"
+                checked={props.item.completed}
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p style={props.item.completed ? completedStyle : null}>{props.item.text}</p>
+        </div>
+    )
+}
+
+export default TodoItem;
---
+++ Todo-list/src/todosData.js
@@ -0,0 +1,33 @@
+const todosData = [
+    {
+        id: "1",
+        text: "Take out the trash",
+        completed: false,
+    },
+
+    {
+        id: "2",
+        text: "Grocery Shopping",
+        completed: false,
+    },
+
+    {
+        id: "3",
+        text: "Clean geco tank",
+        completed: false,
+    },
+
+    {
+        id: "4",
+        text: "Mow lawn",
+        completed: false,
+    },
+
+    {
+        id: "5",
+        text: "Catch up on Arrested development",
+        completed: false,
+    },
+]
+
+export default todosData;
